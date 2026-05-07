d23074d6a1e6e6518b00948dd0ee1917c0ce88aa
Laystoun <timureubov655@gmail.com>
2026-05-07 23:00:41 +0300

    fixed an error extracting the password from the encryption key fingerprint.

--- main.cpp
+++ main.cpp
@@ -437,6 +437,7 @@ void print_rounded_keys(Kyznechik& kyz) {
 }
 
 void key_finger_print(std::array<std::array<uint8_t, 16ULL>, 10ULL>& ROUND_KEY) {
+    SHA256 sha;
     const wchar_t* SHADE[] = {
         L" ",        // 0x00–0x1F
         L"░",        // 0x20–0x5F
@@ -444,20 +445,47 @@ void key_finger_print(std::array<std::array<uint8_t, 16ULL>, 10ULL>& ROUND_KEY) 
         L"▓",        // 0xA0–0xDF
         L"█"         // 0xE0–0xFF
     };
-
-    std::wcout << "KEY FINGERPRINT:" << std::endl << std::endl;
-    for(int key_c = 0; key_c < 10; key_c++) {
+    
+    size_t ROUND_KEY_SIZE = 160;
+    
+    uint8_t key_linear[ROUND_KEY_SIZE];
+    for (int r_key = 0; r_key < 10; r_key++) {
         for (int key_v = 0; key_v < 16; key_v++) {
-            uint8_t hex_v = ROUND_KEY[key_c][key_v];
-
-            if (hex_v >= 0x00 && hex_v <= 0x1F) std::wcout << SHADE[0];
-            if (hex_v <= 0x5F && hex_v >= 0x20) std::wcout << SHADE[1];
-            if (hex_v >= 0x60 && hex_v <= 0x9F) std::wcout << SHADE[2];
-            if (hex_v <= 0xDF && hex_v >= 0xA0) std::wcout << SHADE[3];
-            if (hex_v >= 0xE0 && hex_v <= 0xFF) std::wcout << SHADE[4];
-
+            key_linear[r_key * 16 + key_v] = ROUND_KEY[r_key][key_v];       
         }
-        std::wcout << std::endl;
+    }
+    
+    sha.update(key_linear, 160);
+
+    std::array<uint8_t, 32> key_hash = sha.digest();
+    std::array<uint8_t, 96 + 32> expand_fingerprint;
+
+    for (int f_hash = 0; f_hash < 32; f_hash++) {
+        expand_fingerprint[f_hash] = key_hash[f_hash];
+    }
+
+    for (int ex = 1; ex < 4; ex++) {
+        int rotr = (key_hash[ex] >> 4) & 0x0F;
+        uint8_t rotl_res = (uint8_t)((key_hash[ex] & 0x0F) ^ rotr);
+
+        sha.update(&rotl_res, 1);
+
+        std::array<uint8_t, 32> bit_hash = sha.digest();
+
+        for (int dec = 0; dec < 32; dec++)
+            expand_fingerprint[32 * ex + dec] = bit_hash[dec];
+    }
+
+    std::wcout << "KEY FINGERPRINT (SHA-256):" << std::endl << std::endl;
+    for(int key_c = 0; key_c < 128; key_c++) {
+        uint8_t hex_v = expand_fingerprint[key_c];
+        if (hex_v >= 0x00 && hex_v <= 0x1F) std::wcout << SHADE[0];
+        if (hex_v <= 0x5F && hex_v >= 0x20) std::wcout << SHADE[1];
+        if (hex_v >= 0x60 && hex_v <= 0x9F) std::wcout << SHADE[2];
+        if (hex_v <= 0xDF && hex_v >= 0xA0) std::wcout << SHADE[3];
+        if (hex_v >= 0xE0 && hex_v <= 0xFF) std::wcout << SHADE[4];
+
+        if ((key_c + 1) % 16 == 0) std::wcout << std::endl;
     }
     std::wcout << std::endl;
 }
